bb384c644653d8f3733a20a7a88b06d2aaf762b6
leog <leog91@gmail.com>
2026-01-16 23:55:14 +0000

    added personal routines list, and profile page

--- src/components/FavoriteRoutinesList.tsx
+++ src/components/FavoriteRoutinesList.tsx
@@ -46,7 +46,7 @@ export default async function FavoriteRoutinesList() {
 
   return (
     <Routines
-      name="Your Favorites"
+      name="Favorites"
       routines={favoriteRoutines}
       isLoggedIn={true}
     />
